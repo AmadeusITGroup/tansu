47a74b1f571c18aaa1d6e2d8f5611108b9d5c9fb
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2023-12-08 10:45:41 +0100

    feat: expose default equal function

--- src/index.ts
+++ src/index.ts
@@ -371,6 +371,19 @@ const createEqualCache = (valueIndex: number): Record<number, boolean> => ({
 const skipEqualInSet = Symbol();
 
 /**
+ * Default implementation of the equal function used by tansu when a store
+ * changes, to know if listeners need to be notified.
+ * Returns false if `a` is a function or an object, or if `a` and `b`
+ * are different according to `Object.is`. Otherwise, returns true.
+ *
+ * @param a - First value to compare.
+ * @param b - Second value to compare.
+ * @returns true if a and b are considered equal.
+ */
+export const equal = <T>(a: T, b: T): boolean =>
+  Object.is(a, b) && (!a || typeof a !== 'object') && typeof a !== 'function';
+
+/**
  * Base class that can be extended to easily create a custom {@link Readable} store.
  *
  * @example
@@ -513,7 +526,7 @@ export abstract class Store<T> implements Readable<T> {
    * @returns true if a and b are considered different.
    */
   protected notEqual(a: T, b: T): boolean {
-    return !Object.is(a, b) || (a && typeof a === 'object') || typeof a === 'function';
+    return !equal(a, b);
   }
 
   /**
