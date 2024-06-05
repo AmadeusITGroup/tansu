2d6579258191190394944414b7e7082a665a5591
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-06-05 15:22:58 +0200

    build: allow the package to be used as a git npm dependency

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,56 @@
 import typescript from '@rollup/plugin-typescript';
+import assert from 'assert';
+import { readFile } from 'fs/promises';
+import { defineConfig } from 'rollup';
 
-export default [
-  {
-    output: [
-      {
-        file: './dist/package/index.cjs.js',
-        format: 'cjs',
+const distPackagePrefix = './dist/package/';
+const removeDistPackage = (withDistPackage) => {
+  assert(withDistPackage.startsWith(distPackagePrefix));
+  return `./${withDistPackage.substring(distPackagePrefix.length)}`;
+};
+
+export default defineConfig({
+  output: [
+    {
+      format: 'cjs',
+      file: './dist/package/index.cjs',
+    },
+    {
+      format: 'es',
+      file: './dist/package/index.js',
+    },
+  ],
+  input: './src/index.ts',
+  plugins: [
+    typescript(),
+    {
+      name: 'package',
+      async buildStart() {
+        const pkg = JSON.parse(await readFile('./package.json', 'utf8'));
+        delete pkg.private;
+        delete pkg.devDependencies;
+        delete pkg.scripts;
+        delete pkg.husky;
+        delete pkg.commitlint;
+        delete pkg.files;
+        pkg.typings = removeDistPackage(pkg.typings);
+        pkg.main = removeDistPackage(pkg.main);
+        pkg.module = removeDistPackage(pkg.module);
+        pkg.exports.types = removeDistPackage(pkg.exports.types);
+        pkg.exports.require = removeDistPackage(pkg.exports.require);
+        pkg.exports.default = removeDistPackage(pkg.exports.default);
+        this.emitFile({ type: 'asset', fileName: 'package.json', source: JSON.stringify(pkg) });
+        this.emitFile({
+          type: 'asset',
+          fileName: 'README.md',
+          source: await readFile('README.md', 'utf-8'),
+        });
+        this.emitFile({
+          type: 'asset',
+          fileName: 'LICENSE',
+          source: await readFile('LICENSE', 'utf-8'),
+        });
       },
-      {
-        file: './dist/package/index.es.js',
-        format: 'es',
-      },
-    ],
-    input: './src/index.ts',
-    plugins: [typescript()],
-  },
-];
+    },
+  ],
+});
