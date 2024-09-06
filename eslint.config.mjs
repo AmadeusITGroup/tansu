ece84fcb2fe7da1a79aeda3acb94fa19fb9aea22
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-09-06 17:41:57 +0200

    chore: switch to eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/no-empty-function': 0,
+      '@typescript-eslint/no-non-null-assertion': 0,
+      '@typescript-eslint/explicit-module-boundary-types': 2,
+      '@typescript-eslint/no-unused-vars': 2,
+    },
+  },
+];
