7f3b84d1c28c6a566184f1451b6f6d7f1fde246e
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-11-08 18:43:23 +0100

    Adding benchmarks from https://github.com/milomg/js-reactivity-benchmark

---
+++ benchmarks/js-reactivity-benchmarks/kairo/diamond.bench.ts
@@ -0,0 +1,37 @@
+// adapted from https://github.com/milomg/js-reactivity-benchmark/blob/main/src/kairo/diamond.ts
+
+import { bench, expect } from 'vitest';
+import type { ReadableSignal } from '../../../src';
+import { computed, writable } from '../../../src';
+
+const width = 5;
+
+const head = writable(0);
+const current: ReadableSignal<number>[] = [];
+for (let i = 0; i < width; i++) {
+  current.push(computed(() => head() + 1));
+}
+const sum = computed(() => current.map((x) => x()).reduce((a, b) => a + b, 0));
+let callCounter = 0;
+computed(() => {
+  sum();
+  callCounter++;
+}).subscribe(() => {});
+
+bench(
+  'diamond',
+  () => {
+    head.set(1);
+    expect(sum()).toBe(2 * width);
+    const atleast = 500;
+    callCounter = 0;
+    for (let i = 0; i < 500; i++) {
+      head.set(i);
+      expect(sum()).toBe((i + 1) * width);
+    }
+    expect(callCounter).toBe(atleast);
+  },
+  {
+    throws: true,
+  }
+);
