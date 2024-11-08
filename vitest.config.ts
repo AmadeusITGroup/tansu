83920d940e88e7ecdcbdcbefa34067714401304d
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-11-08 15:59:26 +0100

    Adding some benchmarks

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vitest/config';
+import JsonArrayReporter from './benchmarks/jsonArrayReporter';
 
 export default defineConfig({
   test: {
     setupFiles: ['test.ts'],
-    include: ['./**/*.spec.ts'],
+    include: ['src/**/*.spec.ts'],
     environment: 'happy-dom',
     coverage: {
       provider: 'v8',
       reporter: ['lcov'],
+      include: ['src/**'],
+    },
+    benchmark: {
+      include: ['benchmarks/**/*.bench.ts'],
+      reporters: [new JsonArrayReporter(), 'default'],
     },
   },
 });
